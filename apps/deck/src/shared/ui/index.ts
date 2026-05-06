79b89fc7d6badcbcdaeedfc2a0f9502e371f7b5e
qianshe <cutoffother@gmail.com>
2026-05-07 00:48:03 +0800

    feat：导出 Button / Card / Badge 公共 API

---
+++ apps/deck/src/shared/ui/index.ts
@@ -0,0 +1,10 @@
+export { Button, buttonVariants, type ButtonProps } from "./button";
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./card";
+export { Badge, badgeVariants, type BadgeProps } from "./badge";
